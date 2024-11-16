6c1a4d544c82a0d2a600e60c90dfe0e2c7d0eeab
Nhien Dinh <nhiendinhzhjihon@gmail.com>
2024-11-16 17:46:16 +0700

    feat:accounts

---
+++ admin/controllers/AccountController.php
@@ -0,0 +1,56 @@
+<?php
+class AccountController {
+    private $account;
+    public function __construct() {
+        $this->account = new AccountModel();
+    }
+    public function index() {
+        $accounts = $this->account->getAll();
+        require_once "views/accounts/index.php";
+    }
+    public function show($id) {
+        $accounts = $this->account->getById($id);
+        require_once 'views/accounts/show.php';
+    }
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Kiểm tra sự tồn tại của các key trong $_POST
+            $data = [
+                'user_name' => $_POST['user_name'],
+                'email' => $_POST['email'],
+                'password' => password_hash($_POST['password'],PASSWORD_DEFAULT),
+                'phone' => $_POST['phone'] ,
+                'address' => $_POST['address'],
+                'role_id' => $_POST['role_id']
+            ];
+            $this->account->create($data);
+            header('Location: ?act=accounts');
+        } else {
+            require_once "views/accounts/create.php";
+        }
+    }
+    public function edit($id)  {
+        if($_SERVER['REQUEST_METHOD' === 'POST']) {
+            $data = [
+                'user_name' => $_POST['user_name'],
+                'email' => $_POST['email'],
+                'password' => password_hash($_POST['password'],PASSWORD_DEFAULT),
+                'phone' => $_POST['phone'] ,
+                'address' => $_POST['address'] ,
+                'role_id' => $_POST['role_id'] 
+            ];
+            $this->account->edit($id,$data);
+            header('Location:?act=accounts');
+        }else {
+            $accounts = $this->account->getById($id);
+            require_once 'views/accounts/edit.php';
+        }
+    }
+    public function delete($id)  {
+        $this->account->delete($id);
+        header('Location:?act=accounts');
+    }
+
+}
+
+?>
--- admin/controllers/ProductController.php
+++ admin/controllers/ProductController.php
@@ -49,39 +49,6 @@ class ProductController
         }
     }
 
-    public function edit($id)
-    {
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            $thumbnail = $_POST['thumbnail'] ?? '';
-            if(isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
-                $thumbnail = '../uploads/' . basename($_FILES['image']['name']);
-                move_uploaded_file($_FILES['image']['tmp_name'], $thumbnail);
-            }
-
-            $data = [
-                'name' => $_POST['nameModel'] ?? '',
-                'category_id' => $_POST['categoryModel'] ?? '',
-                'thumbnail' => $thumbnail,
-                'short_description' => $_POST['descriptionModel'] ?? '',
-                'content' => $_POST['contentModel'] ?? '',
-                'status' => $_POST['statusModel'] ?? '',
-                'views' => $_POST['viewsModel'] ?? '',
-                'sale_price' => $_POST['sale_priceModel'] ?? 0,
-                'price' => $_POST['priceModel'] ?? 0
-            ];
-
-            $this->productModel->edit($id, $data);
-            header('Location: ?act=products');
-        }else{
-            $product = $this->productModel->getById($id);
-            require_once 'views/products/edit.php';
-        }
-    }
-    public function delete($id)
-    {
-        $this->productModel->delete($id);
-        header('Location: ?act=products');
-    }
 
 }
 ?>
