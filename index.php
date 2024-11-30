9eacc32f5ba07427d817b67b048f323348c61022
Nhien Dinh <nhiendinhzhjihon@gmail.com>
2024-11-30 16:30:27 +0700

    editIndex

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
 session_start();
- if (!isset($_SESSION['myCart'])) {
+if (!isset($_SESSION['myCart'])) {
     $_SESSION['myCart'] = []; // Khởi tạo giỏ hàng nếu chưa tồn tại
 }
 require_once 'commons/env.php';
 require_once 'commons/function.php';
 
 require_once 'controllers/HomeController.php';
-<<<<<<< HEAD
+require_once 'controllers/AuthController.php';
 
 require_once 'models/cart.php';
-=======
-require_once 'controllers/CartController.php';
-require_once 'controllers/AuthController.php';
->>>>>>> 9b4da3ab907b5b6192e8624333d38446846b2516
 
 require_once 'admin/models/ProductModel.php';
 require_once 'admin/models/CategoryModel.php';
@@ -25,15 +21,15 @@ $act = $_GET['act'] ?? '/';
 match ($act) {
     '/' => (new HomeController())->index(),
 
-    'carts'          => (new HomeController())->cart(),
+    'carts' => (new HomeController())->cart(),
 
     'single-product' => (new HomeController())->show(id: $_GET['id'] ?? 0),
 
-    'checkout'       => (new HomeController()) -> checkout(),
-    'confirm_orders' => (new HomeController()) -> confirm_orders(),
-    
-    'product'         => (new HomeController())->productByCategory(),
-    
+    'checkout' => (new HomeController())->checkout(),
+    'confirm_orders' => (new HomeController())->confirm_orders(),
+
+    'product' => (new HomeController())->productByCategory(),
+
     'single-product/show' => (new HomeController())->show($_GET['id'] ?? 0),
     'register' => (new AuthController())->register(),
     'login' => (new AuthController())->login(),
