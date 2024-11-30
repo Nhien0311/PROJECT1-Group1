9b4da3ab907b5b6192e8624333d38446846b2516
LitteBoyS2CMD <mungnvph52815@gmail.com>
2024-11-30 16:19:33 +0700

    registerComplete

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ require_once 'commons/function.php';
 
 require_once 'controllers/HomeController.php';
 require_once 'controllers/CartController.php';
+require_once 'controllers/AuthController.php';
 
 require_once 'admin/models/ProductModel.php';
 require_once 'admin/models/CategoryModel.php';
 require_once 'models/product.php';
+require_once 'models/user.php';
 
 $act = $_GET['act'] ?? '/';
 
@@ -18,6 +20,9 @@ match ($act) {
     'product' => (new HomeController())->productByCategory(),
     'carts' => (new CartController())->index(),
     'single-product/show' => (new HomeController())->show($_GET['id'] ?? 0),
+    'register' => (new AuthController())->register(),
+    'login' => (new AuthController())->login(),
+    'logout' => (new AuthController())->logout()
 };
 
 ?>
